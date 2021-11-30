93852d8d09739abd459a23cb26222236386d671a
NICKOLAIH SADOWSKI <nsadowsk2-2023@livoniapublicschools.org>
2021-11-30 12:08:37 -0500

    test

--- stdcommands/info/info.go
+++ stdcommands/info/info.go
@@ -14,10 +14,10 @@ var Command = &commands.YAGCommand{
 	Description: "Responds with bot information",
 	RunInDM:     true,
 	RunFunc: func(data *dcmd.Data) (interface{}, error) {
-		info := fmt.Sprintf(`**YAGPDB - Yet Another General Purpose Discord Bot**
+		info := fmt.Sprintf(`**Stranger - A Fully Functional Multi-Purpose Discord Bot**
 This bot focuses on being configurable and therefore is one of the most advanced bots.
 It can perform a range of general purpose functionality (Reddit feeds, various commands, moderation utilities, automoderator functionality and so on) and it's configured through a web control panel.
-The bot is run by Botlabs but is open source (<https://github.com/botlabs-gg/yagpdb>), so if you know Go and want to make some contributions, feel free to make a PR.
+The bot is run by Nsado311 but is open source (<https://github.com/Nsadow311/stranger>), so if you know Go and want to make some contributions, feel free to make a PR. (This is a fork of YAGPDB)
 Control panel: <https://%s/manage>
 				`, common.ConfHost.GetString())
 
