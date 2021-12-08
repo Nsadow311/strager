132fc88af49106e412e7fd6d44dcd9eea52f17d4
Nsadow311 <63483406+Nsadow311@users.noreply.github.com>
2021-12-08 09:53:26 -0500

    Update web.go

--- youtube/web.go
+++ youtube/web.go
@@ -15,7 +15,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/Nsadow311/stranger/common"
+	"github.com/Nsadow311/strangercommon"
 	"github.com/Nsadow311/stranger/common/cplogs"
 	"github.com/Nsadow311/stranger/web"
 	"github.com/jinzhu/gorm"
@@ -46,6 +46,7 @@ type Form struct {
 	DiscordChannel     int64 `valid:"channel,false"`
 	ID                 uint
 	MentionEveryone    bool
+	MentionRole        int64 `valid:"role,true"`
 }
 
 func (p *Plugin) InitWeb() {
@@ -117,7 +118,8 @@ func (p *Plugin) HandleNew(w http.ResponseWriter, r *http.Request) (web.Template
 		return templateData.AddAlerts(web.ErrorAlert("Neither channelid or username specified.")), errors.New("ChannelID and username not specified")
 	}
 
-	sub, err := p.AddFeed(activeGuild.ID, data.DiscordChannel, cID, username, data.MentionEveryone)
+	sub, err := p.AddFeed(activeGuild.ID, data.DiscordChannel, data.YoutubeChannelID, data.YoutubeChannelUser, data.MentionEveryone, data.MentionRole)
+	//sub, err := p.AddFeed(activeGuild.ID, data.DiscordChannel, cID, username, data.MentionEveryone)
 	if err != nil {
 		if err == ErrNoChannel {
 			return templateData.AddAlerts(web.ErrorAlert("No channel by that id/username found")), errors.New("Channel not found")
@@ -183,6 +185,7 @@ func (p *Plugin) HandleEdit(w http.ResponseWriter, r *http.Request) (templateDat
 
 	sub.MentionEveryone = data.MentionEveryone
 	sub.ChannelID = discordgo.StrID(data.DiscordChannel)
+	sub.MentionRole = discordgo.StrID(data.MentionRole)
 
 	err = common.GORM.Save(sub).Error
 	if err == nil {
